4c8ec52d76daeb50c9a3e762585c6ddda217ceea
DeepEyeCrypto <nazzo.np@gmail.com>
2026-06-22 16:21:29 +0530

    fix: import border for NetMirrorScreen

--- app/src/main/java/com/deepeye/musicpro/ui/screens/NetMirrorScreen.kt
+++ app/src/main/java/com/deepeye/musicpro/ui/screens/NetMirrorScreen.kt
@@ -4,6 +4,7 @@
 package com.deepeye.musicpro.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
