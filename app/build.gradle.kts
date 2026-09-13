349dbe8c9ca03a0f46e389e25bb949902756d57f
DeepEyeCrypto <nazzo.np@gmail.com>
2026-09-13 11:23:49 +0530

    chore(release): bump version to v3.0.1.30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.deepeye.musicpro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 30039
-        versionName = "3.0.1.29"
+        versionCode = 30040
+        versionName = "3.0.1.30"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
